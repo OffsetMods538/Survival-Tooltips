76bb1729281edf54959b5371e19f344718c45626
Dave-Thomas Thomson <71213040+OffsetMonkey538@users.noreply.github.com>
2022-11-29 23:00:09 +0200

    Make items have their group in their tooltip

---
+++ src/main/java/io/github/offsetmonkey538/survivaltooltips/mixin/ItemStackMixin.java
@@ -0,0 +1,50 @@
+package io.github.offsetmonkey538.survivaltooltips.mixin;
+
+import net.minecraft.client.item.TooltipContext;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Slice;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+
+import java.util.List;
+
+@Mixin(ItemStack.class)
+public abstract class ItemStackMixin {
+
+    @Shadow public abstract Item getItem();
+
+    @Inject(
+            method = "getTooltip",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Ljava/util/List;add(Ljava/lang/Object;)Z",
+                    shift = At.Shift.AFTER
+            ),
+            slice = @Slice(
+                    from = @At(
+                            value = "INVOKE",
+                            target = "Lnet/minecraft/item/ItemStack;hasCustomName()Z"
+                    ),
+                    to = @At(
+                            value = "INVOKE",
+                            target = "Lnet/minecraft/item/ItemStack;isOf(Lnet/minecraft/item/Item;)Z"
+                    )
+            ),
+            locals = LocalCapture.CAPTURE_FAILHARD
+    )
+    public void survival_tooltips$addCreativeTabToTooltip(@Nullable PlayerEntity player, TooltipContext context, CallbackInfoReturnable<List<Text>> cir, List<Text> list) {
+        ItemGroup group = getItem().getGroup();
+        if (group == null) return;
+        list.add(group.getDisplayName().copy().formatted(Formatting.BLUE));
+    }
+}
