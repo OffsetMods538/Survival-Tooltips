1e5d8e1937bf2b52a427928e7de9f638e97b0348
OffsetMonkey538 <71213040+OffsetMonkey538@users.noreply.github.com>
2023-07-18 19:18:39 +0300

    Update to 1.20

--- src/main/java/top/offsetmonkey538/survivaltooltips/mixin/CreativeInventoryScreenMixin.java
+++ src/main/java/top/offsetmonkey538/survivaltooltips/mixin/CreativeInventoryScreenMixin.java
@@ -1,32 +1,39 @@
 package top.offsetmonkey538.survivaltooltips.mixin;
 
-import com.llamalad7.mixinextras.injector.WrapWithCondition;
+import net.minecraft.client.gui.screen.ingame.AbstractInventoryScreen;
 import net.minecraft.client.gui.screen.ingame.CreativeInventoryScreen;
+import net.minecraft.client.gui.screen.ingame.HandledScreen;
+import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.screen.ScreenHandler;
 import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Slice;
+import org.spongepowered.asm.mixin.injection.Inject;
 
 import java.util.List;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(CreativeInventoryScreen.class)
-public abstract class CreativeInventoryScreenMixin {
+public abstract class CreativeInventoryScreenMixin extends AbstractInventoryScreen<CreativeInventoryScreen.CreativeScreenHandler> {
+    public CreativeInventoryScreenMixin(CreativeInventoryScreen.CreativeScreenHandler screenHandler, PlayerInventory playerInventory, Text text) {
+        super(screenHandler, playerInventory, text);
+    }
 
-    @WrapWithCondition(
-            method = "renderTooltip",
-            at = @At(
-                    value = "INVOKE",
-                    target = "Ljava/util/List;add(ILjava/lang/Object;)V"
-            ),
-            slice = @Slice(
-                    from = @At(
-                            value = "INVOKE",
-                            target = "Ljava/util/Set;forEach(Ljava/util/function/Consumer;)V"
-                    )
-            )
+
+    @Shadow private static ItemGroup selectedTab;
+
+    @Inject(
+            method = "getTooltipFromItem",
+            at = @At("HEAD"),
+            cancellable = true
     )
     @SuppressWarnings("unused")
-    public boolean survival_tooltips$removeCreativeTabFromSearchTooltip(List<Text> instance, int index, Object element) {
-        return false;
+    public void survival_tooltips$removeCreativeTabFromSearchTooltip(ItemStack stack, CallbackInfoReturnable<List<Text>> cir) {
+        if (selectedTab.getType() != ItemGroup.Type.SEARCH || client == null) return;
+
+        cir.setReturnValue(HandledScreen.getTooltipFromItem(this.client, stack));
     }
 }
