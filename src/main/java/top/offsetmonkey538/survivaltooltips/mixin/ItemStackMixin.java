30df2f5978ddc75a81586f26fc2bc7e2a4ab4d46
OffsetMonkey538 <71213040+OffsetMonkey538@users.noreply.github.com>
2024-06-10 16:18:40 +0300

    Support 1.20 to 1.21

--- src/main/java/top/offsetmonkey538/survivaltooltips/mixin/ItemStackMixin.java
+++ src/main/java/top/offsetmonkey538/survivaltooltips/mixin/ItemStackMixin.java
@@ -1,6 +1,8 @@
 package top.offsetmonkey538.survivaltooltips.mixin;
 
 import com.llamalad7.mixinextras.sugar.Local;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemGroups;
@@ -16,28 +18,42 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.List;
 
-@Mixin(ItemStack.class)
+@Mixin(
+        remap = false,
+        value = ItemStack.class
+)
 public abstract class ItemStackMixin {
 
     @Inject(
-            method = "getTooltip",
+            method = {
+                    // 1.21 intermediary
+                    "method_7950(Lnet/minecraft/class_1792$class_9635;Lnet/minecraft/class_1657;Lnet/minecraft/class_1836;)Ljava/util/List;",
+                    // 1.21 yarn
+                    "getTooltip(Lnet/minecraft/item/Item$TooltipContext;Lnet/minecraft/entity/player/PlayerEntity;Lnet/minecraft/item/tooltip/TooltipType;)Ljava/util/List;",
+                    // 1.21 mojmap
+                    "getTooltipLines(Lnet/minecraft/world/item/Item$TooltipContext;Lnet/minecraft/world/entity/player/Player;Lnet/minecraft/world/item/TooltipFlag;)Ljava/util/List;",
+
+                    // 1.20 intermediary
+                    "method_7950(Lnet/minecraft/class_1657;Lnet/minecraft/class_1836;)Ljava/util/List;",
+                    // 1.20 yarn
+                    "getTooltip(Lnet/minecraft/entity/player/PlayerEntity;Lnet/minecraft/client/item/TooltipContext;)Ljava/util/List;",
+                    // 1.20 mojmap
+                    "getTooltipLines(Lnet/minecraft/world/entity/player/Player;Lnet/minecraft/world/item/TooltipFlag;)Ljava/util/List;"
+            },
             at = @At(
                     value = "INVOKE",
                     target = "Ljava/util/List;add(Ljava/lang/Object;)Z",
                     shift = At.Shift.AFTER
             ),
             slice = @Slice(
-                    from = @At(
-                            value = "INVOKE",
-                            target = "Lcom/google/common/collect/Lists;newArrayList()Ljava/util/ArrayList;"
-                    ),
                     to = @At(
+                            remap = true,
                             value = "INVOKE",
                             target = "Lnet/minecraft/item/ItemStack;isOf(Lnet/minecraft/item/Item;)Z"
                     )
             )
     )
-    public void survival_tooltips$addCreativeTabToTooltip(CallbackInfoReturnable<List<Text>> cir, @Local List<Text> list, @Local(argsOnly = true) @Nullable PlayerEntity player) {
+    public void survival_tooltips$addCreativeTabToTooltip(CallbackInfoReturnable<List<Text>> cir, @SuppressWarnings("LocalMayBeArgsOnly") @Local List<Text> list, @Local(argsOnly = true) @Nullable PlayerEntity player) {
         final MinecraftClient client = MinecraftClient.getInstance();
         if (client == null) return;
         if (!(player instanceof ClientPlayerEntity clientPlayer)) return;
