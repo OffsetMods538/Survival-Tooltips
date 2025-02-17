13c450cdda7acdc98c896f1f5a5da50a326e29b0
OffsetMonkey538 <71213040+OffsetMonkey538@users.noreply.github.com>
2025-02-17 17:49:41 +0200

    Support versions 1.20 to 1.21.4
    Had to use a @Group to allow one of the injects to fail. 1.20 didn't have the `isAdvanced` thing so last commit was lying :(

--- src/main/java/top/offsetmonkey538/survivaltooltips/mixin/ItemStackMixin.java
+++ src/main/java/top/offsetmonkey538/survivaltooltips/mixin/ItemStackMixin.java
@@ -11,9 +11,8 @@ import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Slice;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.*;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.List;
@@ -24,15 +23,34 @@ import java.util.List;
 )
 public abstract class ItemStackMixin {
 
+    @Group(name = "survival_tooltips$addCreativeTabToTooltip", min = 1, max = 1)
     @Inject(
             method = {
-                    // 1.21 intermediary
+                    // 1.21 and .4 intermediary
                     "method_7950(Lnet/minecraft/class_1792$class_9635;Lnet/minecraft/class_1657;Lnet/minecraft/class_1836;)Ljava/util/List;",
-                    // 1.21 yarn
+                    // 1.21 and .4 yarn
                     "getTooltip(Lnet/minecraft/item/Item$TooltipContext;Lnet/minecraft/entity/player/PlayerEntity;Lnet/minecraft/item/tooltip/TooltipType;)Ljava/util/List;",
-                    // 1.21 mojmap
-                    "getTooltipLines(Lnet/minecraft/world/item/Item$TooltipContext;Lnet/minecraft/world/entity/player/Player;Lnet/minecraft/world/item/TooltipFlag;)Ljava/util/List;",
+                    // 1.21 and .4 mojmap
+                    "getTooltipLines(Lnet/minecraft/world/item/Item$TooltipContext;Lnet/minecraft/world/entity/player/Player;Lnet/minecraft/world/item/TooltipFlag;)Ljava/util/List;"
+            },
+            at = @At(
+                    value = "INVOKE",
+                    target = "Ljava/util/List;add(Ljava/lang/Object;)Z",
+                    shift = At.Shift.AFTER
+            ),
+            slice = @Slice(to = @At(
+                    remap = true,
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/item/tooltip/TooltipType;isAdvanced()Z"
+            ))
+    )
+    public void survival_tooltips$addCreativeTabToTooltip121(CallbackInfoReturnable<List<Text>> cir, @Local List<Text> list, @Local(argsOnly = true) @Nullable PlayerEntity player) {
+        survival_tooltips$addCreativeTabToTooltip(list, player);
+    }
 
+    @Group(name = "survival_tooltips$addCreativeTabToTooltip", min = 1, max = 1)
+    @Inject(
+            method = {
                     // 1.20 intermediary
                     "method_7950(Lnet/minecraft/class_1657;Lnet/minecraft/class_1836;)Ljava/util/List;",
                     // 1.20 yarn
@@ -45,15 +63,18 @@ public abstract class ItemStackMixin {
                     target = "Ljava/util/List;add(Ljava/lang/Object;)Z",
                     shift = At.Shift.AFTER
             ),
-            slice = @Slice(
-                    to = @At(
-                            remap = true,
-                            value = "INVOKE",
-                            target = "Lnet/minecraft/item/ItemStack;isOf(Lnet/minecraft/item/Item;)Z"
-                    )
-            )
+            slice = @Slice(to = @At(
+                    remap = true,
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/item/ItemStack;isOf(Lnet/minecraft/item/Item;)Z"
+            ))
     )
-    public void survival_tooltips$addCreativeTabToTooltip(CallbackInfoReturnable<List<Text>> cir, @SuppressWarnings("LocalMayBeArgsOnly") @Local List<Text> list, @Local(argsOnly = true) @Nullable PlayerEntity player) {
+    public void survival_tooltips$addCreativeTabToTooltip120(CallbackInfoReturnable<List<Text>> cir, @Local List<Text> list, @Local(argsOnly = true) @Nullable PlayerEntity player) {
+        survival_tooltips$addCreativeTabToTooltip(list, player);
+    }
+
+    @Unique
+    public void survival_tooltips$addCreativeTabToTooltip(List<Text> list, @Nullable PlayerEntity player) {
         final MinecraftClient client = MinecraftClient.getInstance();
         if (client == null) return;
         if (!(player instanceof ClientPlayerEntity clientPlayer)) return;
